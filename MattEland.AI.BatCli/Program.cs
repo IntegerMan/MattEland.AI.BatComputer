411b3d00097cd28bdb7b1e573c711866801b5303
Matt Eland <Matt.Eland@GMail.com>
2025-04-15 20:56:58 -0400

    Added a CLI project and Microsoft.Extensions.AI

---
+++ MattEland.AI.BatCli/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MattEland.AI.BatLogic;
+using Microsoft.Extensions.AI;
+using Spectre.Console;
+
+IAnsiConsole console = AnsiConsole.Console;
+
+// Figlet logo
+console.Write(new FigletText("BatCLI"));
+console.MarkupLine("[bold green]By Matt Eland[/]");
+console.WriteLine();
+
+IChatClient chatClient = new EchoChatClient();
+
+// Get a message from the user
+console.MarkupLine("[yellow]Enter a message to send to the echo chat client:[/]");
+string userInput = console.Ask<string>($"[green]{ChatRole.User}:[/]");
+
+ChatMessage message = new(ChatRole.User, userInput);
+ChatResponse response = await chatClient.GetResponseAsync(message);
+
+foreach (ChatMessage responseMessage in response.Messages)
+{
+    console.MarkupLine($"[cyan]{responseMessage.Role}:[/] {responseMessage.Text}");
+}
---
+++ MattEland.AI.BatLogic/Class1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.AI;
+
+namespace MattEland.AI.BatLogic;
+
+public class EchoChatClient : IChatClient
+{
+    public Task<ChatResponse> GetResponseAsync(IEnumerable<ChatMessage> messages, ChatOptions? options = null,
+        CancellationToken cancellationToken = new())
+    {
+        IEnumerable<ChatMessage> responseMessages = messages.Select(m =>
+        {
+            List<AIContent> responses = m.Contents.ToList();
+            return new ChatMessage(ChatRole.System, responses);
+        });
+
+        return Task.FromResult(new ChatResponse(responseMessages.ToList()));
+    }
+
+    public IAsyncEnumerable<ChatResponseUpdate> GetStreamingResponseAsync(IEnumerable<ChatMessage> messages,
+        ChatOptions? options = null,
+        CancellationToken cancellationToken = new())
+    {
+        throw new NotImplementedException();
+    }
+
+    public object? GetService(Type serviceType, object? serviceKey = null)
+    {
+        if (serviceType == typeof(IChatClient))
+        {
+            return this;
+        }
+
+        return null;
+    }
+    
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
